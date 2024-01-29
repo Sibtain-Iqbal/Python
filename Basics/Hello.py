d7ef4fff8645a4e49c06fb9096b1821747dbb23b
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-01-29 09:56:49 -0800

    Initial commit

---
+++ Basics/Hello.py
@@ -0,0 +1,11 @@
+print("heelo python")
+
+def chai(p):
+    print(p)
+
+chai("heello my chai")    
+
+def lemon(si) :
+    print(si)
+
+lemon("this is lemon")    
---
+++ Basics/chai.py
@@ -0,0 +1,4 @@
+from Hello import chai
+from Hello import lemon
+chai(" import chai from hello")
+lemon("import port lemon from hello")
